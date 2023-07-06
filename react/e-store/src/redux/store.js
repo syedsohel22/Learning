5956c3136b0a629c290582fff45004a9fa5c5ac9
sohel <mr.sohelsyed@gmail.com>
2023-07-06 10:59:39 +0530

    routing

---
+++ react/e-store/src/redux/store.js
@@ -0,0 +1,8 @@
+import { legacy_createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers({});
+
+const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
