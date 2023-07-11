eceb0fab85b26a4d45d442639450ebd93d46a52b
sohel <mr.sohelsyed@gmail.com>
2023-07-11 19:42:13 +0530

    changes

--- react/react-project/src/main.jsx
+++ react/react-project/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
