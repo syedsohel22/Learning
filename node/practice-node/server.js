073d5239ab70b3a29da43421f4f3163462ff63de
sohel <mr.sohelsyed@gmail.com>
2023-07-23 22:34:08 +0530

    practing node

--- node/practice-node/server.js
+++ node/practice-node/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
 const app = express();
-const port = 5000;
+require("dotenv").config();
+const port = process.env.port || 5000;
+mongoose.connect(process.env.altaspath);
 app.listen(port, () => {
   console.log("server listening on port", port);
 });
