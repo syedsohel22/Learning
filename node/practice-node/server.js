32bc2eb9e93cf5fb8515813905e632567cd3406d
sohel <mr.sohelsyed@gmail.com>
2023-07-24 22:25:20 +0530

    server file added

--- node/practice-node/server.js
+++ node/practice-node/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
-const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
-const port = process.env.port || 5000;
-mongoose.connect(process.env.altaspath);
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
 app.listen(port, () => {
-  console.log("server listening on port", port);
+  console.log("listening on port " + port);
 });
