37cb1fb88200ec1197f21d72a11f2e249fdc6c68
sohel <mr.sohelsyed@gmail.com>
2023-08-05 23:44:59 +0530

    mathmatical operators

--- python/inputout.py
+++ python/inputout.py
@@ -1,2 +1,74 @@
-name=input("Enter your name: "),
-print("hello ", name)
+
+
+
+
+
+name = input("Enter your name: ")
+#name = input("enter your name: ")
+age= int(input("Enter your age: "))
+
+print("hello " + name + "! you are " + str(age) + "years old")
+
+
+# # Input from the user
+# age = int(input("Enter your age: "))  # Converting the input to an integer
+
+# # Output to the console
+# print("Hello, " + name + "! You are " + str(age) + " years old.")
+
+a = int(input("enter the nubmer a as value: "))
+b = int(input("enter the nubmer b as value: "))
+
+print("the sum is", a+b)
+print("the product is", a*b)
+
+a = 5
+b = 10
+result = a + b
+print(result)  # Output: 15
+
+a = 20
+b = 7
+result = a - b
+print(result)  # Output: 13
+
+a = 3
+b = 4
+result = a * b
+print(result)  # Output: 12
+
+a = 15
+b = 3
+result = a / b
+print(result)  # Output: 5.0 (result is a float)
+
+a = 15
+b = 4
+result = a // b
+print(result)  # Output: 3 (result is an integer, rounded down)
+
+a = 17
+b = 5
+result = a % b
+print(result)  # Output: 2 (remainder of 17 divided by 5)
+
+a = 2
+b = 3
+result = a ** b
+print(result)  # Output: 8 (2 raised to the power of 3)
+
+# Calculate the area of a rectangle
+length = 5
+width = 3
+area = length * width
+print("Area of the rectangle:", area)
+
+# Calculate the volume of a cube
+side_length = 4
+volume = side_length ** 3
+print("Volume of the cube:", volume)
+
+# Convert temperature from Celsius to Fahrenheit
+celsius = 30
+fahrenheit = (celsius * 9/5) + 32
+print("Temperature in Fahrenheit:", fahrenheit)
