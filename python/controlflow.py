bdd8a65ccc9f76f427354b19a804c7eabb8b2f09
sohel <mr.sohelsyed@gmail.com>
2023-08-05 23:13:03 +0530

    basics

---
+++ python/controlflow.py
@@ -0,0 +1,17 @@
+age= int(input("enter your age here: "))
+if age>=18:
+    print("You are an adult")
+else:
+    print("You are a minor")
+
+
+#for loop
+fruits = ["apple", "orange", "cherry"]
+for f in fruits:
+    print(f)
+
+#while loop
+count=1
+while count<=5:
+    print(count)
+    count+=1
---
+++ python/datatypes.py
@@ -0,0 +1,17 @@
+a= 10
+b = "apple"
+c = True
+d= ["apple", "banana", "charry","orange"]
+e={"name":"sohel", "address":"india"}
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+age= 30
+pi= 3.14
+name= "james"
+is_active=True
+
+print(age,pi,name,is_active)
