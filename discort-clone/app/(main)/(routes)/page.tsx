3daa5b697b53bafbff2276201161cac03ffae660
sohel <mr.sohelsyed@gmail.com>
2023-09-10 19:39:44 +0530

    changes

--- discort-clone/app/(main)/(routes)/page.tsx
+++ discort-clone/app/(main)/(routes)/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <p className="text-3xl font-bold text-indigo-500">Hello Discord clone</p>
-      <Button variant={"ghost"}>Click me</Button>
+      <Button variant={"outline"}>Click me</Button>
     </div>
   );
 }
