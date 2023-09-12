1452a158c2d992ff04cced888a6332bed05bb681
sohel <mr.sohelsyed@gmail.com>
2023-09-12 23:30:36 +0530

    authuntication with clerk

--- discort-clone/app/(main)/(routes)/page.tsx
+++ discort-clone/app/(main)/(routes)/page.tsx
@@ -1,10 +1,3 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
-  return (
-    <div>
-      <p className="text-3xl font-bold text-indigo-500">Hello Discord clone</p>
-      <Button variant={"outline"}>Click me</Button>
-    </div>
-  );
+  return <div>this is a protected route.</div>;
 }
