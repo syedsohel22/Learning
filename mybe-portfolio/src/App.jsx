880caeffef0cccff9f0d01275b65b443e14c7701
sohel <mr.sohelsyed@gmail.com>
2023-09-15 23:57:04 +0530

    basic components for page

--- mybe-portfolio/src/App.jsx
+++ mybe-portfolio/src/App.jsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import GithubStat from "./components/GithubStat";
+import Projects from "./components/Projects";
 
 function App() {
-  return <>welcome</>;
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <GithubStat />
+      <Projects />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ mybe-portfolio/src/components/GithubStat.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GithubStat = () => {
+  return <div>GithubStat</div>;
+};
+
+export default GithubStat;
