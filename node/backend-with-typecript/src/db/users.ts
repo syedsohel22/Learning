5a1f14ef3750d7bf3289a5ea7a3088393d8a6cd6
sohel <mr.sohelsyed@gmail.com>
2023-09-19 23:59:26 +0530

    added mongose schema and model with some actions

--- node/backend-with-typecript/src/db/users.ts
+++ node/backend-with-typecript/src/db/users.ts
@@ -1,3 +1,20 @@
 import mongoose from "mongoose";
 
-const UserSchema = mongoose.Schema({});
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  authentication: {
+    password: { type: String, required: true, select: false },
+    salt: { type: String, select: false },
+    sessionToken: { type: String, select: false },
+  },
+});
+
+export const UserModel = mongoose.model("User", UserSchema);
+
+export const getUsers = () => UserModel.find();
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserBySessionToken = (sessionToken: string) =>
+  UserModel.findOne({ "authentication.sessionToken": sessionToken });
+
+export const getUserById = (id: string) => UserModel.findById(id);
