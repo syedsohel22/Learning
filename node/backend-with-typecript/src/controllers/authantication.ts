9dc2a6c2f952ef49de319e350c83390b9cd88212
sohel <mr.sohelsyed@gmail.com>
2023-09-22 23:47:25 +0530

    authentication route

--- node/backend-with-typecript/src/controllers/authantication.ts
+++ node/backend-with-typecript/src/controllers/authantication.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { createUser, getUserByEmail } from "db/users";
-import { random ,authentication} from "./../helpers/index";
+import { random, authentication } from "./../helpers/index";
 
 export const register = async (req: express.Request, res: express.Response) => {
   try {
@@ -18,14 +18,15 @@ export const register = async (req: express.Request, res: express.Response) => {
     const salt = random();
 
     const user = await createUser({
-        email,username
-        authentication:{
-            salt,
-            password: authentication(salt,password),
-        }
-    })
+      email,
+      username,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      },
+    });
 
-    return res.status(200).json(user).end()
+    return res.status(200).json(user).end();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
