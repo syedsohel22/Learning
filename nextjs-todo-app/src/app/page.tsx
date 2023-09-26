e8decb4f2d4cf74ac203abd0de0e0c8132e60820
sohel <mr.sohelsyed@gmail.com>
2023-09-26 13:48:54 +0530

    added globle styles

--- nextjs-todo-app/src/app/page.tsx
+++ nextjs-todo-app/src/app/page.tsx
@@ -1,10 +1,24 @@
-export default function Home() {
+import Link from "next/link";
+import { prisma } from "./db";
+
+export default async function Home() {
+  const todos = await prisma.todo.findMany();
   return (
     <>
-      <header>
+      <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl"> Todos</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/new"
+        >
+          New
+        </Link>
       </header>
-      <ul></ul>
+      <ul className="pl-4">
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
     </>
   );
 }
