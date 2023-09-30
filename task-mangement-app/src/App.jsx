921ad92dca8a76531cd25e6d4ca95e9e2bc44638
sohel <mr.sohelsyed@gmail.com>
2023-09-30 13:08:03 +0530

    Project set up

---
+++ task-mangement-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import MainRoutes from "./components/MainRoutes";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <MainRoutes />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ task-mangement-app/src/components/MainRoutes.jsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Profile from "../pages/Profile";
+
+const MainRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
+};
+
+export default MainRoutes;
