ab4b965ee8cfc04c11705515fb0042ee218815f5
sohel <mr.sohelsyed@gmail.com>
2023-10-01 17:34:07 +0530

    added maintask table in home page

--- task-mangement-app/src/pages/Home.jsx
+++ task-mangement-app/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { Box } from "@chakra-ui/react";
 import UserBanner from "../components/UserBanner";
+import MainTaskTable from "../components/MainTaskTable";
 const Home = () => {
   return (
     <Box px={10}>
       <UserBanner />
+      <MainTaskTable />
     </Box>
   );
 };
