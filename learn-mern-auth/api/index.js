b41b327f2e7749c97016a848c3358f59554a6316
sohel <mr.sohelsyed@gmail.com>
2023-10-16 17:58:42 +0530

    connect to DB

--- learn-mern-auth/api/index.js
+++ learn-mern-auth/api/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log(`connecting to MONGODB`);
+  })
+  .catch((err) => console.log(err));
 const app = express();
 app.listen(3000, () => {
   console.log(`server listening on 3000`);
