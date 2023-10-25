ca67a60a5bfb2190af2ed92263c6147449c9d931
sohel <mr.sohelsyed@gmail.com>
2023-10-25 23:25:07 +0530

    working on register route

--- mern-chat/api/index.js
+++ mern-chat/api/index.js
@@ -4,24 +4,38 @@ require("dotenv").config();
 const port = 4000;
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 mongoose.connect(process.env.MONGO_URL);
 const jwtSecret = process.env.JWT_SECRET;
 const { userModel } = require("./models/User.Models");
 /******************************************************************************************************************************************** */
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
 
+app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Hello!" });
 });
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  const createdUser = await userModel.create({ username, password });
+  try {
+    const createdUser = await userModel.create({ username, password });
 
-  jwt.sign({ userId: createdUser._id }, jwtSecret, (err, token) => {
-    if (err) throw err;
-    res.cookie("chat-token", token).status(201).json({ message: "your created okay" });
-  });
+    jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
+      if (err) throw err;
+      res
+        .cookie("chat-token", token)
+        .status(201)
+        .json({ message: "your created okay" });
+    });
+  } catch (error) {
+    res.json({ error: error });
+  }
 });
 
 /******************************************************************************************************************************************** */
--- mern-chat/client/src/pages/Register.jsx
+++ mern-chat/client/src/pages/Register.jsx
@@ -1,11 +1,19 @@
+import axios from "axios";
 import { useState } from "react";
 
 const Register = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  console.log(username, password);
+
+  async function handleRegister(e) {
+    e.preventDefault();
+    await axios.post("/register", { username, password });
+  }
+
   return (
     <div className="bg-blue-50 h-screen flex items-center">
-      <form className="w-64 mx-auto mb-12">
+      <form className="w-64 mx-auto mb-12" onSubmit={handleRegister}>
         <input
           value={username}
           onChange={(e) => setUsername(e.target.value)}
