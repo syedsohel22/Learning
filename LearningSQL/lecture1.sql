ff24f6b5e2f94d5cabe0f2f4d19fb18f7bf6fd3f
sohel <mr.sohelsyed@gmail.com>
2023-11-05 20:37:46 +0530

    updated commands

--- LearningSQL/lecture1.sql
+++ LearningSQL/lecture1.sql
@@ -1,12 +1,13 @@
-CREATE DATABASE test_DB;
 
 
-USE test_DB;
+USE xyz;
 
-CREATE TABLE user (
+CREATE TABLE employee (
     id INT PRIMARY KEY,
     name VARCHAR(100),
     salary INT
 );
 
-select * from user
+INSERT INTO employee (id, name,salary) values(1,"Adam",25000),(2,"Bob",30000),(3,"Don",40000);
+
+select * from employee;
