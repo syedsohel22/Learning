c32cd6ebc3a596be695f7d8d19f21e252d2d218e
Sohel Syed <mr.sohelsyed@gmail.com>
2024-01-03 14:31:09 +0530

    practice with test tables

--- LearningSQL/SQL-in-one-short/file-1.sql
+++ LearningSQL/SQL-in-one-short/file-1.sql
@@ -18,3 +18,39 @@ USE college;
 
 -- SELECT * FROM student;
 
+-- CREATE DATABASE xyz;
+
+-- use xyz;
+
+-- CREATE TABLE employees(
+-- id INT PRIMARY KEY,
+-- name VARCHAR(100),
+-- salary INT
+-- );
+
+-- INSERT INTO employees (id,name,salary) values (1,"adam",24000),(2,"bob",50000);
+-- SELECT * FROM employees;
+
+-- CREATE TABLE student (
+-- rollon INT PRIMARY KEY,
+-- name VARCHAR(50),
+-- marks INT NOT NULL,
+-- grade VARCHAR(1),
+-- city VARCHAR(20)
+-- );
+
+-- SELECT * FROM student;
+
+-- INSERT INTO student (rollon,name,marks,grade,city)
+-- VALUES
+-- (101,'anil',78,'c','Pune'),
+-- (102,'bhumika',93,'a','Mumbai'),
+-- (103,'chetan',85,'b','Mumbai'),
+-- (104,'dhruv',96,'a','Delhi'),
+-- (105,'emanual',12,'f','Delhi'),
+-- (106,'farah',82,'b','Delhi');
+
+-- SELECT name,marks FROM student;
+
+-- SELECT * FROM student where marks>80;
+-- SELECT * FROM student where city = 'mumbai'
