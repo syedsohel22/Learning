c908e3357bed4ca9921b4aa2e1c1e8979352b428
Sohel Syed <mr.sohelsyed@gmail.com>
2024-02-02 23:55:39 +0530

    Javascript mastery course notes

---
+++ Javascript/complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,62 @@
+// console.log("hello world");
+// let firstName = "John";
+// // console.log(firstName);
+
+// console.log(true);
+// let javascriptIsFun = true;
+
+// console.log(javascriptIsFun);
+// console.log(typeof javascriptIsFun); //boolean
+// console.log(typeof 23); //number
+// console.log(typeof "John"); //string
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 2020;
+// console.log(year);
+// console.log(typeof year);
+
+// Primitive Data Types
+
+// 1.String
+// 2.Number
+// 3.Boolean
+// 4.Undefined
+// 5.Null
+// 6.Symbol
+
+// let,var and const
+
+// let age = 30;
+// console.log(age);
+// age = 32;
+// console.log(age);
+// const birthYear = 1996;
+// console.log(birthYear);
+// var job = "programmer";
+// console.log(job);
+
+// Operators in JavaScript
+
+// Arithmetic Operators (+,-,*,/,%,**)
+// logical operator (&& , || , !)
+// comparison operator ( <,> <=,>=,!=,==,===)
+// assignment operators (=)
+
+// let today = "Monday";
+
+// if (today === "Monday") {
+//   console.log("Today is " + today);
+// } else {
+//   console.log("Today is not " + today);
+// }
+
+// today = "Friday";
+
+// if (today === "Friday") {
+//   console.log(`Today is ${today}`);
+// } else {
+//   console.log(`today is not ${today}`);
+// }
