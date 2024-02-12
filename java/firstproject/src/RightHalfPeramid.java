1f1d013ad44e42c256b9d17c4b9482f004556dc6
Sohel Syed <mr.sohelsyed@gmail.com>
2024-02-12 23:42:52 +0530

    stepper component

---
+++ java/firstproject/src/RightHalfPeramid.java
@@ -0,0 +1,13 @@
+public class RightHalfPeramid {
+    public static void main(String[] args) {
+        System.out.println(" *");
+        System.out.println(" *");
+
+        System.out.println(" * * *");
+        System.out.println(" *");
+        System.out.println(" *");
+
+
+
+    }
+}
