28b090bce972de1c2031de0297ffbf16e37402fd
Sohel Syed <mr.sohelsyed@gmail.com>
2024-02-13 00:54:40 +0530

    basic stepper ui

--- JavaScript/React/stepper/src/App.jsx
+++ JavaScript/React/stepper/src/App.jsx
@@ -1,5 +1,30 @@
+import CheckoutStepper from "./components/Stepper";
+import "./App.css";
 const App = () => {
-  return <div>App</div>;
+  const CHECKOUT_STEPSE = [
+    {
+      name: "Customer Info",
+      component: () => <div>Provide your contact details</div>,
+    },
+    {
+      name: "Shipping Info",
+      component: () => <div>Provide your shipping details</div>,
+    },
+    {
+      name: "Payment",
+      component: () => <div>Complete payment for your details</div>,
+    },
+    {
+      name: "Delivered",
+      component: () => <div>your order has been deliverd</div>,
+    },
+  ];
+  return (
+    <>
+      <h2>Checkout</h2>
+      <CheckoutStepper stepsConfig={CHECKOUT_STEPSE} />
+    </>
+  );
 };
 
 export default App;
--- JavaScript/React/stepper/src/components/Stepper.jsx
+++ JavaScript/React/stepper/src/components/Stepper.jsx
@@ -1,5 +1,67 @@
-const Stepper = () => {
-  return <div>Stepper</div>;
+import { useState } from "react";
+
+const CheckoutStepper = ({ stepsConfig = [] }) => {
+  const [currentStep, setCurrentStep] = useState(2);
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  if (!stepsConfig.length) {
+    return <></>;
+  }
+
+  const handleNext = () => {
+    setCurrentStep((prevStep) => {
+      if (prevStep === stepsConfig.length) {
+        setIsCompleted(true);
+        return prevStep;
+      } else {
+        return prevStep + 1;
+      }
+    });
+  };
+
+  const calculateProgressBar = () => {
+    return ((currentStep - 1) / (stepsConfig.length - 1)) * 100;
+  };
+  return (
+    <>
+      <div className="stepper">
+        {stepsConfig.map((step, index) => {
+          return (
+            <div
+              key={step.name}
+              className={`step ${
+                currentStep > index + 1 || isCompleted ? "complete" : ""
+              } ${currentStep === index + 1 ? "active" : ""}`}
+            >
+              <div className="step-number">
+                {currentStep > index + 1 || isCompleted ? (
+                  <span>&#10003;</span>
+                ) : (
+                  index + 1
+                )}
+              </div>
+              <div className="step-name">{step.name}</div>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="progress-bar">
+        <div
+          className="progress"
+          style={{ width: `${calculateProgressBar()}%` }}
+        ></div>
+      </div>
+
+      <div>
+        {!isCompleted && (
+          <button className="btn" onClick={handleNext}>
+            {currentStep === stepsConfig.length ? "Finish" : "next"}
+          </button>
+        )}
+      </div>
+    </>
+  );
 };
 
-export default Stepper;
+export default CheckoutStepper;
