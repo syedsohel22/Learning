77b497ce54cc595b41871e81d704e35b32a3b154
Sohel Syed <mr.sohelsyed@gmail.com>
2024-08-27 09:38:39 +0530

    python shell pratice

---
+++ python/chai-aur-python/01_basics/hello_chai.py
@@ -0,0 +1,28 @@
+print("chai aur python")
+
+
+def chai(n):
+    print(n)
+
+chai(4)
+
+
+# shell
+
+# python3
+# Python 3.12.3 (main, Jul 31 2024, 17:43:48) [GCC 13.2.0] on linux
+# Type "help", "copyright", "credits" or "license" for more information.
+# >>> 4+4
+# 8
+# >>> "chai" * 4
+# 'chaichaichaichai'
+# >>> score =100
+# >>> score
+# 100
+# >>> import os
+# >>> os.getcwd()
+# '/home/sohel/code/Learning/python/chai-aur-python/01_basics'
+# >>> import sys
+# >>> sys.platform
+# 'linux'
+# >>> 
