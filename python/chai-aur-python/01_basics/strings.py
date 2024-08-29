3592b22f2032ee04c7c94bb8d20094febd9f7768
Sohel Syed <mr.sohelsyed@gmail.com>
2024-08-29 22:25:20 +0530

    python strings

---
+++ python/chai-aur-python/01_basics/strings.py
@@ -0,0 +1,66 @@
+# here are some string methods
+
+# negative index allowed in python strings
+
+a = "hello"
+
+print(a[-1])
+
+# slicing
+
+print(a[0:3])
+
+# concatenation
+
+a = "hello"
+b = "world"
+
+print(a + " " + b)  
+
+# repetition
+
+print(a * 3)
+
+# membership
+
+print("h" in a)
+
+# length
+
+print(len(a))   
+
+# capitalize
+
+print(a.capitalize())
+
+# casefold
+
+print(a.casefold())
+
+# count
+
+print(a.count("l"))
+
+# find
+
+print(a.find("l"))
+
+# index
+
+print(a.index("l"))
+
+# isalnum
+
+print(a.isalnum())  
+
+# uppercase
+
+print(a.upper())
+
+# lowercase
+
+print(a.lower())
+
+
+
+
