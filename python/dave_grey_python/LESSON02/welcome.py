f844805fccbb55df5c6cd845df9b01e28c101ff5
Sohel Syed <mr.sohelsyed@gmail.com>
2024-09-03 23:57:07 +0530

    basics lessons

---
+++ python/dave_grey_python/LESSON02/welcome.py
@@ -0,0 +1,29 @@
+# variable name convetions
+
+# dont use reserved words as variable names
+
+# dont use special characters in variable names
+
+# dont use spaces in variable names
+
+# dont use numbers in variable names
+
+# dont use underscores in variable names
+
+
+# statement vs expression
+
+# statement
+# statement is a line of code which does not return any value to the program itself
+
+# example
+# my_name = "sohel"
+# print(my_name)
+
+
+# expression
+# expression is a line of code which returns a value to the program itself
+
+# example
+# toal = 4 + 4
+# print(toal)
