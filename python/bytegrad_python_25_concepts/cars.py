3fe6dce61d53b5098b5a031e277f882d87cdb5fd
Sohel Syed <mr.sohelsyed@gmail.com>
2025-02-25 15:18:55 +0530

    changes

--- python/bytegrad_python_25_concepts/cars.py
+++ python/bytegrad_python_25_concepts/cars.py
@@ -1,19 +1,19 @@
 # Data Types
 brand = "Toyota"
 model_year = 2022
-mileage  = 202323.50
+mileage = 202323.50
 is_insured = True
 cars = ["Toyota", "Hondo", "Ford"]
 car_info = {
-    "brand":"Toyota",
-    "year":2022,
-    "color":"red"
+    "brand": "Toyota",
+    "year": 2022,
+    "color": "red"
 }
 # string interpolation
-car_info_string = "The car is" +" "+ "30 years old"
+car_info_string = "The car is" + " " + "30 years old"
 
 car_age = 30
-car_info_string1 = "The car is " +str(car_age)+ " years old"
+car_info_string1 = "The car is " + str(car_age) + " years old"
 # formatted strings
 car_info_string2 = f"The car is {car_age} years old"
 
@@ -23,13 +23,13 @@ print(car_info_string2)
 
 # Math Operator
 
-x  = 5+3 #Addition
-y = 10-7 # Subtraction
-z= 4*2 # Multiplication
-div = 10/2 # Division (float division)
-floor_div = 10//3 # floor division
-exp = 2**3 # exponent
-mod = 10 %3 # Modulus (remainder)
+x = 5+3  # Addition
+y = 10-7  # Subtraction
+z = 4*2  # Multiplication
+div = 10/2  # Division (float division)
+floor_div = 10//3  # floor division
+exp = 2**3  # exponent
+mod = 10 % 3  # Modulus (remainder)
 
 
 # List operation accessing, adding, removing, manupulating
@@ -46,7 +46,7 @@ print(car_info["year"])
 
 # snace case
 
-max_speed  =1000
+max_speed = 1000
 
 # constants (ALL CAPS)
 
@@ -57,35 +57,76 @@ print(max_speed)
 
 # Functions
 
+
 def start_cars():
     print("Attempting to start toyota car..")
     print("Attempting to start Honda car..")
 
+
 start_cars()
 
 # function call with parameter and arguments
+
+
 def start_cars1(car_1, car_2):
     print(f"Attempting to start {car_1} car..")
     print(f"Attempting to start {car_2} car..")
 
+
 start_cars1("Honda", "Tata")
 
 # default parameter
-def start_cars2(car_1 ="Tesla", car_2="BMW"):
+
+
+def start_cars2(car_1="Tesla", car_2="BMW"):
     print(f"Attempting to start {car_1} car..")
     print(f"Attempting to start {car_2} car..")
+
 
 start_cars2()
 
 # function return
 
-def sum(a,b):
+
+def sum(a, b):
     return a+b
 
-val_sum= sum(2,2)
+
+val_sum = sum(2, 2)
 
 print(val_sum)
 
 # pass (this will not cause error)
+
+
 def new_fun():
     pass
+
+# conditional
+
+
+speed = 65
+
+if speed > 55:
+    print("you are driveing above the speed limit")
+elif speed == 55:
+    print("you are driveing at speed limit")
+else:
+    print("you are driveing below the speed limit")
+
+
+# turnory
+status = "above limit" if speed > 55 else "within limit"
+
+print(status)
+
+
+# loops
+
+for car in cars:
+    print("I love", car)
+
+# range
+
+for i in range(3):
+    print(i)
