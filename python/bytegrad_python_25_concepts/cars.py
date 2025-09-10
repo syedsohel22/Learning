910a4cede1ac12931eb9e0756daaded889e6930b
Sohel Syed <mr.sohelsyed@gmail.com>
2025-09-10 23:00:10 +0530

    list operations

--- python/bytegrad_python_25_concepts/cars.py
+++ python/bytegrad_python_25_concepts/cars.py
@@ -21,6 +21,8 @@ print(car_info_string)
 print(car_info_string1)
 print(car_info_string2)
 
+# Math Operator
+
 x  = 5+3 #Addition
 y = 10-7 # Subtraction
 z= 4*2 # Multiplication
@@ -28,3 +30,16 @@ div = 10/2 # Division (float division)
 floor_div = 10//3 # floor division
 exp = 2**3 # exponent
 mod = 10 %3 # Modulus (remainder)
+
+
+# List operation accessing, adding, removing, manupulating
+print(cars[1])
+print(cars)
+cars.append("Tata")
+print(cars)
+cars.remove("Hondo")
+print(cars)
+
+print(car_info["year"])
+car_info["year"] = 2050
+print(car_info["year"])
