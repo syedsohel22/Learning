7ac8c1a372f91717f80175a9e1c4a3efdc4a2468
Sohel Syed <mr.sohelsyed@gmail.com>
2025-09-10 22:38:48 +0530

    data types, string interpolation, formatted strings

--- python/bytegrad_python_25_concepts/cars.py
+++ python/bytegrad_python_25_concepts/cars.py
@@ -9,10 +9,12 @@ car_info = {
     "year":2022,
     "color":"red"
 }
-
+# string interpolation
 car_info_string = "The car is" +" "+ "30 years old"
+
 car_age = 30
 car_info_string1 = "The car is " +str(car_age)+ " years old"
+# formatted strings
 car_info_string2 = f"The car is {car_age} years old"
 
 print(car_info_string)
