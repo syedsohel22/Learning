c273107976b088aa64e35825a0cebcd20f55e36c
Sohel Syed <mr.sohelsyed@gmail.com>
2025-09-29 22:20:09 +0530

    learning bash scripting

---
+++ bash/networkchuck/bestdayever.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo 'Good Morning !'
+sleep 1
+echo 'Have a nice day'
+sleep 1
+echo 'you look very good today'
---
+++ bash/networkchuck/himom1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo 'hi mom'
+
+sleep 3
+
+echo 'Um uh'
+sleep 3
+
+echo 'oh wow'
+sleep 3
+
+echo 'thats great'
+sleep 3
+
+echo 'Bye Love you too.!'
